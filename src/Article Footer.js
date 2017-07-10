3c8e5fc4cd22b513d00faea05493586f9b544a02
Lillian Gray <lmgray16@earlham.edu>
2017-07-10 11:36:58 -0400

    Added comment box that displays when comments button is clicked

--- src/Article Footer.js
+++ src/Article Footer.js
@@ -2,17 +2,27 @@ import React, { Component } from 'react'
 import './Article Footer.css'
 
 class ArticleFooter extends Component {
+    handleClick = () => {
+      const Item = document.getElementById('comment-box')
+      Item.classList.toggle('hidden')
+    }
+
     render() {
         return(
+        <div>
         <div className="article-links">
-              <a className="article-link" href="#">
-                <i className="fa fa-comments-o"></i>
-                <span className="article-link-text">Comments</span>
-              </a>
-              <a className="article-link" href="#">
-                <i className="fa fa-share"></i>
-                <span className="article-link-text">Share Post</span>
-              </a>
+          <a className="article-link" href="#" onClick={this.handleClick}>
+            <i className="fa fa-comments-o"></i>
+            <span className="article-link-text">Comments</span>
+          </a>
+          <a className="article-link" href="#">
+            <i className="fa fa-share"></i>
+            <span className="article-link-text">Share Post</span>
+          </a>
+        </div>
+        <form id="comment-box" className="hidden">
+            <input type="text" name="comment" placeholder="Add a comment and press enter to submit" />
+        </form>
         </div>
         )
     }
--- src/Other Articles.js
+++ src/Other Articles.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react'
 import './Other Articles.css'
 
+let articleLinks = [
+  {imageURL:"https://www.enclavepublishing.com/wp-content/uploads/2015/11/tumblr_inline_mtvwr6T4qH1r8eb4v.gif",
+  title:"Single Orcs in Indianapolis" }, 
+  {imageURL:"https://images.fineartamerica.com/images-medium-large-5/rocky-mountain-landscape-meadow-in-spring-western-panorama-with-wildflowers-square-format-walt-curlee.jpg",
+  title:"You won't believe what's under this mountain" }, 
+  {imageURL:"http://orig01.deviantart.net/a278/f/2010/357/9/7/seamless___gold_coins_by_bartalon-d35iydr.jpg",
+  title:"Mine 20% more gold with One Weird Trick"}, 
+  {imageURL:"http://cdn23.us1.fansshare.com/photos/thehobbit/the-hobbit-square-characters-833282558.jpg",
+  title:"Surprise for Indiana Hobbits born before 1999" }
+  ]
+
 class ArticleLink extends Component {
     render() {
         return(
@@ -17,6 +28,12 @@ class ArticleLink extends Component {
 class OtherArticles extends Component {
     render() {
         return(
+        // {articleLinks.map((articleLink) => {
+        //       <ArticleLink
+        //       imageURL=articleLink.imageURL
+        //       title=articleLinks.title
+        //       />
+        // })}
         <div className="small-12 columns other-articles">
             <h2>From around the Realm</h2>
             <ArticleLink
